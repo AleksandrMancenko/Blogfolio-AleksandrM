b8f352f97fba7f7580acd352daf1f9077e0bd9c2
Aleksandrs <mancenko@gmail.com>
2025-09-08 18:02:04 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Component } from './Component.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Component name='Alex' age={40}/>
   </React.StrictMode>
 );
 
